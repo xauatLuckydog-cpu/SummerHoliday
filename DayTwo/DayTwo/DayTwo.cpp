14094861ddfa650bb6d7c7170398ac63b86e8ca9
hupangpang <305997735@qq.com>
2019-07-27 11:32:49 +0800

    二分、快排、归并等

--- DayTwo/DayTwo/DayTwo.cpp
+++ DayTwo/DayTwo/DayTwo.cpp
@@ -1,20 +1,28 @@
-﻿// DayTwo.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include <vector>
+using namespace std;
 
-#include <iostream>
+class Solution {
+public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
+		if (matrix.empty() || matrix[0].empty()) return false;
+
+		int n = matrix.size(), m = matrix[0].size();
+		int l = 0, r = n * m + 1;
+		while (l < r) {
+			int mid = l + r >> 1;
+			if (matrix[mid / m][mid % m] >= target) r = mid;
+			else l = mid + 1;
+		}
+		if (matrix[r / m][r % m] != target) return false;
+		return true;
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
-}
+	int i = 1;
+	int j = i++ + ++i;
+	std::cout << j;
+}	
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
